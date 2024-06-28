9a91821860258e781f5a6a8a8feec930fccf0664
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-06-28 23:47:44 +0300

    I have installed django setup two apps and done a simple landing page

---
+++ sales_managnment/cashier/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from cashier import views
+
+urlpatterns = [
+    path('sing-up/', views.sign_up, name='sign-up'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('record-sale/', views.record_sale, name='record-sale'),
+]
