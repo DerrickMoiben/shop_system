5812d8b9572f972b4e1925cb42b3e9276fa60539
derrickkimtai <derrickmoio92@gmail.com>
2024-06-29 23:11:25 +0300

    'imprved'

--- sales_managnment/cashier/urls.py
+++ sales_managnment/cashier/urls.py
@@ -4,7 +4,7 @@ from cashier import views
 
 urlpatterns = [
     path('sing-up/', views.sign_up, name='sign-up'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('record-sale/', views.record_sale, name='record-sale'),
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
+    # path('record-sale/', views.record_sale, name='record-sale'),
 ]
