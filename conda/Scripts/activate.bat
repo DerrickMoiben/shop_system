a8d45545fb225b79e40ab95f0025792c9f0198b6
Isaiah Kibet <156502124+Isa8205@users.noreply.github.com>
2024-08-16 22:30:23 +0300

    Reviving
    Made a new virtual environment

--- conda/Scripts/activate.bat
+++ conda/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Owner\Documents\Github\shop_system\conda
+set VIRTUAL_ENV=C:\Users\HP 830 G5\OneDrive\Documents\GitHub\shop_system\conda
 
 if not defined PROMPT set PROMPT=$P$G
 
